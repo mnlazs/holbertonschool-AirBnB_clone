7f3244b50bd882846319b094152fe67b7eb23967
LW068 <105394574+LW068@users.noreply.github.com>
2023-02-07 14:04:25 -0600

    Update ___init__.py

--- models/___init__.py
+++ models/___init__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-from models.engine import file_storage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+""" Import modules and packages """
+from models.engine.file_storage import FileStorage
 
-storage = file_storage.FileStorage()
+storage = FileStorage()
 storage.reload()
