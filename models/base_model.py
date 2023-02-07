fd069bfa3b2a213b94700a2810f6473be6037f50
mnlazs <mnl.azs@gmail.com>
2023-02-06 16:00:45 -0800

    load

--- models/base_model.py
+++ models/base_model.py
@@ -28,12 +28,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
+        """Metodo de cadena
+        """
+        class_name = self.__class__.__name__
+        id_string = self.id
+        my_dict = self.__dict__
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         obj_dict = self.__dict__.copy()
