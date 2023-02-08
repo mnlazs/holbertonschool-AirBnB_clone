2f1cc1a5a82e3f8659269891069e2bccdd506bdf
LW068 <105394574+LW068@users.noreply.github.com>
2023-02-07 21:54:55 -0600

    [Fixed]: E305 console.py

--- console.py
+++ console.py
@@ -161,7 +161,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class doesn't exist **")
 
 if __name__ == '__main__':
+
+
     HBNBCommand().cmdloop()
-
-
-    """ placeholder for pycodestyle"""
+   
